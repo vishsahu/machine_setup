b3f687cb2d2fa1da9b1bbeeda483d9f846fb003a
vishal sahu <vishal.sahu@isilon.com>
2017-05-01 14:20:28 -0700

    modified setup file

--- setup.sh
+++ setup.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
-rm $HOME/.bashrc
-rm $HOME/.vimrc
-rm $HOME/.profile
-ln -s $PWD/.bashrc $HOME/.bashrc
-ln -s $PWD/.vimrc $HOME/.vimrc
-ln -s $PWD/.profile $HOME/.profile
-ln -s $PWD/.vim $HOME/.vim
-ln -s $PWD/cpp_template.txt $HOME/cpp_template.txt
-ln -s $PWD/c_template.txt $HOME/c_template.txt
-ln -s $PWD/git-prompt.sh $HOME/git-prompt.sh
+declare -a names=("c_template.txt"
+                  "cpp_template.txt"
+                  "git-prompt.sh"
+                  ".bashrc"
+                  ".profile"
+                  ".vimrc"
+                  ".vim"
+                  )
+
+# stores all the exisiting files here
+if ! [ -d $HOME/mconfig_backup ]; then
+    mkdir $HOME/mconfig_backup
+fi
+
+for fname in "${names[@]}"
+do
+    # check if symlink is not present
+    if ! [ -L $HOME/$fname ]; then
+        # if existing file is directory or regular file
+        if [ -f $HOME/$fname ] || [ -d $HOME/$fname ]; then
+            mv $HOME/$fname $HOME/mconfig_backup
+        fi
+        ln -s $PWD/$fname $HOME/$fname
+    fi
+done
+
 source $HOME/.profile
